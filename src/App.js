71083ad7269eac7ed9eda6e6ff390a13fc63561a
Ben-Hadji <83819844+Ben-Hadji@users.noreply.github.com>
2022-11-25 23:12:53 +0100

    let's go

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import Menu from './composants/Menu';
 import Calendrier from './composants/Calendrier';
 function App() {
   return (
-    // <Fragment className="bg-warning">
-    //   <div className="container">
-    //     <Calendrier/>
-    //   </div>
-    // </Fragment>
     <div className="App">
       <Menu/>
       <header className="App-header">
